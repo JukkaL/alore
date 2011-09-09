046c3026003a79e6a21ccf43ae3148cde7563d95
Jukka Lehtosalo <jukka.lehtosalo@iki.fi>
2011-09-09 11:50:16 +0100

    Refactor interpreter main program
    Also fix potential integer size issue.

--- src/main.c
+++ src/main.c
@@ -19,141 +19,57 @@
 #define USAGE "Usage: alore [option] ... programfile [arg] ..."
 
 
+typedef struct {
+    ABool displayCode;
+    ABool checksDefined;
+    ABool checkFirstDefined;
+    Asize_t maxHeap;
+} AOptions;
+
+
+static void ParseOptions(int *argcp, char ***argvp, AOptions *options);
+static void InitializeOptions(AOptions *options);
 static void InvalidOption(const char *opt);
 static void InvalidOptionValue(const char *opt, const char *value);
 static void ShowHelp(void);
 static void ShowVersion(void);
-static ABool ParseSize(const char *s, unsigned long *size);
+static ABool ParseSize(const char *s, Asize_t *size);
 
 
 /* The main function of the Alore interpreter. */
 int main(int argc, char **argv)
 {
     int num;
-    ABool disp;
     int returnValue;
     AThread *t;
     char *file;
     char *interp;
-    unsigned long maxHeap = 0;
-
-    /* Some options are only available in builds with extra debugging
-       features. */ 
-    
-#ifdef A_DEBUG    
-    ABool checksDefined = FALSE;
-    ABool checkFirstDefined = FALSE;
-#endif
+    AOptions options;
 
     interp = argv[0];
 
+    /* Skip interpreter program path (argument 0). */
     argv++;
     argc--;
 
-    returnValue = 0;
-    file = NULL;
-    disp = FALSE;
-    
-    while (argc > 0 && argv[0][0] == '-') {
-        switch (argv[0][1]) {
-#ifdef A_DEBUG
-        case 'd':
-            /* Display code */
-            disp = TRUE;
-            break;
-#endif
-        case 'v':
-            ShowVersion();
-            break;
-
-        case '-':
-            /* Long argument with -- prefix. */
-            if (strcmp(argv[0], "--version") == 0) {
-                ShowVersion();
-                break;
-            } else if (strcmp(argv[0], "--max-heap") == 0 && argc > 1) {
-                /* Set the maximum size of the heap. */
-                ABool status = ParseSize(argv[1], &maxHeap);
-                if (!status)
-                    InvalidOptionValue(argv[0], argv[1]);
-                argv++;
-                argc--;
-                break;
-            } else
-                InvalidOption(argv[0]);
-            
-#ifdef A_DEBUG
-        case 'm':
-            /* Memory dump (FIX: what does it mean) */
-            ADebug_MemoryDump = 1;
-            break;
-            
-        case 't':
-            /* Memory trace (FIX: what does it mean) */
-            ADebug_MemoryTrace = 1;
-            break;
-            
-        case 'a': {
-            /* Set up debug watch location (FIX: what does it mean) */
-            unsigned long ptr;
-            argv++;
-            argc--;
-
-            if (argc > 0 && sscanf(argv[0], "%lx", &ptr) == 1) {
-                ADebug_WatchLocation = (void *)ptr;
-                break;
-            }
-        }
-
-        case 'D':
-            /* Set periodic instruction checking options. */
-            checksDefined = TRUE;
-            switch (argv[0][2]) {
-            case 'n':
-                /* Debug every Nth instruction. */
-                argv++;
-                argc--;
-                ADebugCheckEveryNth = atoi(argv[0]);
-                if (ADebugCheckEveryNth < 1)
-                    ADebugCheckEveryNth = 1;
-                break;
-            case 'f':
-                /* Set first instruction to check. */
-                argv++;
-                argc--;
-                ADebugCheckFirst = atoi(argv[0]);
-                checkFirstDefined = TRUE;
-                break;
-            case 'l':
-                /* Set last instruction to check. */
-                argv++;
-                argc--;
-                ADebugCheckLast = atoi(argv[0]);
-                break;
-            default:
-                InvalidOption(argv[0]);
-            }
-            break;
-#endif
-        default:
-            InvalidOption(argv[0]);
-        }
-        argv++;
-        argc--;
-    }
+    ParseOptions(&argc, &argv, &options);
         
     if (argc >= 1) {
         /* Get the name of the program to run. */
         file = argv[0];
         argv++;
         argc--;
-    } else
+    } else {
+        /* Program name missing. Display error and exit. */
         InvalidOption(NULL);
+        /* Get rid of warning (never executed). */
+        file = NULL;
+    }
 
 #ifdef A_DEBUG
     /* If any periodic check related parameters are defined, also update
        DebugCheckFirst (otherwise the parameters would have no effect). */
-    if (checksDefined && !checkFirstDefined) {
+    if (options.checksDefined && !options.checkFirstDefined) {
 #ifdef A_DEBUG_COMPILER
         ADebugCheckFirst = 0;
 #else
@@ -161,12 +77,14 @@ int main(int argc, char **argv)
 #endif
     }
 #endif
+    
+    returnValue = 0;
 
     num = ALoadAloreProgram(&t, file, interp, "", FALSE, argc, argv, NULL,
-                            maxHeap);
+                            options.maxHeap);
     if (num >= 0) {
 #ifdef A_DEBUG        
-        if (disp)
+        if (options.displayCode)
             ADisplayCode();
         else
 #endif
@@ -194,6 +112,127 @@ int main(int argc, char **argv)
     }
 
     return returnValue;
+}
+
+
+/* Parse command line options. Update *argcp and *argvp to point to the first
+   positional argument. Fully initialize and populate the options struct.
+
+   Note that some options are only available in builds with extra debugging
+   features.
+
+   This function does not return if one of options is invalid or is completely
+   processed by this function (e.g. -v). */
+static void ParseOptions(int *argcp, char ***argvp, AOptions *options)
+{
+    int argc = *argcp;
+    char **argv = *argvp;
+
+    InitializeOptions(options);
+
+    /* Parse options; advance argc and argv. */
+    while (argc > 0 && argv[0][0] == '-') {
+        switch (argv[0][1]) {
+#ifdef A_DEBUG
+        case 'd':
+            /* Display code and exit. */
+            options->displayCode = TRUE;
+            break;
+#endif
+        case 'v':
+            ShowVersion();
+            break;
+
+        case '-':
+            /* Long argument with -- prefix. */
+            if (strcmp(argv[0], "--version") == 0) {
+                ShowVersion();
+                break;
+            } else if (strcmp(argv[0], "--max-heap") == 0 && argc > 1) {
+                /* Set the maximum size of the heap. */
+                ABool status = ParseSize(argv[1], &options->maxHeap);
+                if (!status)
+                    InvalidOptionValue(argv[0], argv[1]);
+                argv++;
+                argc--;
+                break;
+            } else
+                InvalidOption(argv[0]);
+            
+#ifdef A_DEBUG
+        case 'm':
+            /* Memory dump (IDEA: explain) */
+            ADebug_MemoryDump = 1;
+            break;
+            
+        case 't':
+            /* Memory trace (IDEA: explain) */
+            ADebug_MemoryTrace = 1;
+            break;
+            
+        case 'a': {
+            /* Set up debug watch location (IDEA: explain) */
+            unsigned long ptr;
+            argv++;
+            argc--;
+
+            if (argc > 0 && sscanf(argv[0], "%lx", &ptr) == 1) {
+                ADebug_WatchLocation = (void *)ptr;
+                break;
+            }
+        }
+
+        case 'D':
+            /* Set periodic instruction checking options. */
+            options->checksDefined = TRUE;
+            switch (argv[0][2]) {
+            case 'n':
+                /* Debug every Nth instruction. */
+                argv++;
+                argc--;
+                ADebugCheckEveryNth = atoi(argv[0]);
+                if (ADebugCheckEveryNth < 1)
+                    ADebugCheckEveryNth = 1;
+                break;
+            case 'f':
+                /* Set first instruction to check. */
+                argv++;
+                argc--;
+                ADebugCheckFirst = atoi(argv[0]);
+                options->checkFirstDefined = TRUE;
+                break;
+            case 'l':
+                /* Set last instruction to check. */
+                argv++;
+                argc--;
+                ADebugCheckLast = atoi(argv[0]);
+                break;
+            default:
+                InvalidOption(argv[0]);
+            }
+            break;
+#endif
+        default:
+            InvalidOption(argv[0]);
+        }
+        
+        argv++;
+        argc--;
+    }
+
+    /* Update caller's argument information. */
+    *argcp = argc;
+    *argvp = argv;
+}
+
+
+/* Initialize options struct to default values. */
+static void InitializeOptions(AOptions *options)
+{
+    options->checksDefined = FALSE;
+    options->checkFirstDefined = FALSE;
+    options->displayCode = FALSE;
+    options->maxHeap = 0;
 }
 
 
@@ -248,11 +287,11 @@ static void ShowVersion(void)
 /* Parse sizes such as 4M or 123k and store them in *size.
 
    NOTE: Does not check overflow on 64-bit systems. */
-static ABool ParseSize(const char *s, unsigned long *size)
+static ABool ParseSize(const char *s, Asize_t *size)
 {
     char *end;
     long sval = strtol(s, &end, 10);
-    unsigned long val;
+    Asize_t val;
 
     if (end == s || sval < 0) {
         return FALSE;
